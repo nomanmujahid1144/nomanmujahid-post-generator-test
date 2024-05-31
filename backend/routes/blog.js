2efcb66b25566581fdec393e22f3d04680170777
nomanmujahid1144 <nomanmujahid.cs@gmail.com>
2024-05-31 19:08:21 +0500

    Complete Readme File

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
     createBlogPosts,
-} = require('../controllers/blog.controllers')
+} = require('../controllers/generatePost.controllers')
 
 router.post('/create-blog-post', createBlogPosts)
 
